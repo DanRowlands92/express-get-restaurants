aa32aead173498a555ef0ca96eb0ff45001141f0
DanRowlands92 <daniel.rowlands@barclays.com>
2024-05-20 12:45:21 +0100

    First Commit

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
+const res = require("express/lib/response");
 
 //TODO: Create your GET Request Route Below: 
+app.get("/restaurants", async (req, res) =>{
+    const restaurants = await Restaurant.findAll();
 
+    res.json(restaurants);
+})
 
 
 
